623bb344de5a6b5595611fd23bd50446bb704c36
Fredrik Holm <karlfredrikholm@gmail.com>
2022-11-18 12:50:42 +0100

    so far so good

--- src/1-multiplication.spec.js
+++ src/1-multiplication.spec.js
@@ -4,4 +4,7 @@ describe('1-multiplication', () => {
   it('should return the value from multiplying two numbers', () => {
     expect(multiply(2, 2)).toEqual(4)
   })
+  it('test', () => {
+    expect(multiply(3, 3)).toEqual(9)
+  })
 })
